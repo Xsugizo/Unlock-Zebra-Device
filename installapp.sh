a16b3b940dfd63f1a8ac8ed0ef610dd7e5b23228
Xsugizo <120628998+Xsugizo@users.noreply.github.com>
2023-12-20 16:53:08 +0800

    Add files via upload

---
+++ installapp.sh
@@ -0,0 +1,5 @@
+sudo apt install python3-tk #tkinder
+pip3 install selenium #selenium
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb #chrome
+sudo dpkg -i google-chrome-stable_current_amd64.deb #install  chrome
+sudo pip3 install PyAutoGUI
